5f41181e928484908b922810ff1760a6dcab20f0
trevore777 <40551345+trevore777@users.noreply.github.com>
2026-08-30 11:25:38 +1000

    Add enquiry page SEO metadata

---
+++ app/enquire/metadata.js
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: "Discuss Your App Idea",
+  description: "Tell EDU Apps Plus about your app idea, workflow problem or existing software project. Start a conversation about custom app and software development.",
+  alternates: { canonical: "/enquire" },
+};
