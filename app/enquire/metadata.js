8207cc687580b711bb5713f5ac9e6e4b4efa8df1
trevore777 <40551345+trevore777@users.noreply.github.com>
2026-08-30 11:25:46 +1000

    Remove unused enquiry metadata file

--- app/enquire/metadata.js
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-  title: "Discuss Your App Idea",
-  description: "Tell EDU Apps Plus about your app idea, workflow problem or existing software project. Start a conversation about custom app and software development.",
-  alternates: { canonical: "/enquire" },
-};
