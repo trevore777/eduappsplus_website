ed781ffb77a42f58c5e8eb6f5f492c748819d186
trevore777 <40551345+trevore777@users.noreply.github.com>
2026-08-30 11:24:31 +1000

    Add XML sitemap routes

---
+++ app/sitemap.js
@@ -0,0 +1,29 @@
+import { caseStudies } from "../data/caseStudies";
+
+const baseUrl = "https://www.eduappsplus.com.au";
+
+export default function sitemap() {
+  const staticRoutes = [
+    "",
+    "/app-development",
+    "/education-apps",
+    "/small-business-apps",
+    "/have-an-idea",
+    "/case-studies",
+    "/enquire",
+  ].map((path) => ({
+    url: `${baseUrl}${path}`,
+    lastModified: new Date(),
+    changeFrequency: path === "" ? "weekly" : "monthly",
+    priority: path === "" ? 1 : path === "/enquire" ? 0.7 : 0.8,
+  }));
+
+  const caseStudyRoutes = caseStudies.map((study) => ({
+    url: `${baseUrl}/case-studies/${study.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
+
+  return [...staticRoutes, ...caseStudyRoutes];
+}
